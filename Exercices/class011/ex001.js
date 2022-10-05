d0f81f41bf7a0a88a297b93df45b6df364057205
AlineR4 <aline-d-r@hotmail.com>
2022-10-05 18:46:12 -0300

    exercices

---
+++ Exercices/class011/ex001.js
@@ -0,0 +1,6 @@
+var vel = 62
+console.log (`The velocity of your car is ${vel} km/h`)
+if (vel > 60) {
+    console.log('you exceeded the permitted speed, fined!')
+}
+console.log('Always drive safely!')
---
+++ Exercices/class011/ex009.js
@@ -0,0 +1,7 @@
+var país = 'EUA'
+if (país== 'Brasil') {
+    console.log('Brazilian!')
+}
+    else {
+    console.log('Exchange!')
+}
