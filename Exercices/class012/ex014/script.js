d1b4d196797ea45d9514fa8128a01c413e71d8c9
AlineR4 <aline-d-r@hotmail.com>
2022-10-17 18:38:00 -0300

    exercices

--- Exercices/class012/ex014/script.js
+++ Exercices/class012/ex014/script.js
@@ -1,18 +1,22 @@
 function loading(){ 
 var msg = window.document.getElementById('msg')
-var img = window.document.getElementById('image')
+var img = window.document.getElementById('imagem')
 var date = new Date()
 var hour = date.getHours()
-msg.innerHTML = `Its now ${hour} o'clock`
+
+msg.innerHTML = `It's now ${hour} o'clock`
 if (hour >= 0 && hour < 12 ) {
     // Good Morning
-    img.src = 'morning.jpg'
+    img.src = 'images/morning.jpg'
+    document.body.style.background = '#e2cd9f'
 } else if (hour >= 12 && hour < 18){
     // Good Afternoon
-    img.src = 'afternoon.jpg'
+    img.src = 'images/afternoon.jpg'
+    document.body.style.background = '#b9846f'
 
 } else {
     // Good Evening
     img.src = 'images/evening.jpg'
+    document.body.style.background = '#515154'
 }
 }
